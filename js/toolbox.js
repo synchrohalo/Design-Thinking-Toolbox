ae3c3c7a32ef5d37294dee13a411f0389d53438a
Joanne Arboleda <jbarboleda@ucdavis.edu>
2015-03-02 04:36:15 -0800

    override back button

--- js/toolbox.js
+++ js/toolbox.js
@@ -1,6 +1,31 @@
+var curPage = '#toolbox-page';
+var prevPage = '#toolbox-page';
+
+
 $(document).ready(function() {
 	$('#mind-map').on({ 'click' : function(){ 
 		$( "#toolbox-page" ).hide();
 		$( "#tut-page" ).show("fast");
+		curPage = '#tut-page';
+		prevPage = '#toolbox-page';
 	} });
 });
+
+/*function showScreen(){
+      $(curPage).hide();
+      $(".screen").show();
+      $(document).on("backbutton", onBackButton);
+}*/
+
+function hideScreen(){
+      $(curPage).hide();
+      $(prevPage).show();
+      $(document).off("backbutton", onBackButton);
+}
+
+function onBackButton(){
+      hideScreen();
+	  var temp = curPage;
+	  curPage = prevPage;
+	  prevPage = temp;
+}
